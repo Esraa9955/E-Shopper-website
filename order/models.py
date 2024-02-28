2d04d1827979ddd11207041e13c919ef961133b4
Esraa Abd El_Maksoud <abdelmaksoudesraa99@gmail.com>
2024-02-28 22:11:06 +0200

    implement order modils

--- order/models.py
+++ order/models.py
@@ -1,15 +1,59 @@
 from django.db import models
 from product.models import Product
+from operator import mod
+from django.db import models
+from django.contrib.auth.models import User
 from users.models import Customer,DeliveryMan
-
+from django.contrib.auth import get_user_model
 import datetime
+UserModel = get_user_model()
 class Order(models.Model):
-  product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-  customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-  delivery_man_id = models.ForeignKey(DeliveryMan,on_delete=models.CASCADE)
-  quantity = models.IntegerField(default=1)
-  total_price = models.DecimalField(default=0,decimal_places=2,max_digits=6)
-  #payment_method = models.ForeignKey(Payment,on_delete=models.CASCADE)
-  date = models.DateField(default=datetime.datetime.today)
-  status = models.BooleanField(default=False)
+  PENDING_STATE='P'
+  SHIPPED_STATE='S'
+  DELIVERD_STATE='D'
+  ORDER_STATUS_CHOICES = ((PENDING_STATE,"pending"),(SHIPPED_STATE,"shipped"),(DELIVERD_STATE,"deliverd"))
 
+  #buyer =  models.ForeignKey(UserModel,related_name="order", on_delete=models.CASCADE)
+  order_number = models.CharField(max_length=250, blank=True, null=True)
+  #delivery_man_id = models.ForeignKey(DeliveryMan,on_delete=models.CASCADE)
+  #payment_method = models.ForeignKey(Payment,on_delete=models.CASCADE)
+  #placed_at= models.DateField(auto_now_add=True)
+  status = models.CharField(
+        max_length=50, choices=ORDER_STATUS_CHOICES, default=PENDING_STATE
+    )
+  is_paid = models.BooleanField(default=False)
+  #address = models.ForeignKey(
+  #      Address, related_name="order_address", on_delete=models.CASCADE
+  #  )
+
+  @staticmethod
+  def create_order(buyer, order_number, address, is_paid=False):
+      order = Order()
+      order.buyer = buyer
+      order.order_number = order_number
+      #order.address = address
+      order.is_paid = is_paid
+      order.save()
+      return order
+  
+
+class OrderItem(models.Model):
+  order = models.ForeignKey(
+      Order, related_name="order_items", on_delete=models.CASCADE
+  )
+  product = models.ForeignKey(
+      Product, related_name="product_order", on_delete=models.CASCADE
+  )
+  quantity = models.IntegerField()
+  total_price = models.DecimalField(default=0,decimal_places=2,max_digits=6)
+
+
+  @staticmethod
+  def create_order_item(order, product, quantity, total):
+    order_item = OrderItem()
+    order_item.order = order
+    order_item.product = product
+    order_item.quantity = quantity
+    order_item.total = total
+    order_item.save()
+    return order_item
--- order/views.py
+++ order/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Order
 
-# Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+  serializer_class = Order.objects.all()
