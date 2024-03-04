df211381bcb5ab8785b43f7791403b7953253c72
Hanaa Gamal <elshriefhanaa@gmail.com>
2024-03-04 03:13:41 +0200

    sale ,new , new price for product

--- product/models.py
+++ product/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from category.models import Category
 from django.conf import settings
+from django.core.exceptions import ValidationError
+
+
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255,default="",blank=False)
@@ -13,10 +16,18 @@ class Product(models.Model):
     brand = models.CharField(max_length=225,default="",blank=False)
     stock=models.IntegerField(default=0)
     ratings = models.DecimalField(max_digits=3,decimal_places=2,default=0)
-  
+    new = models.BooleanField(default=False)
+    sale = models.BooleanField(default=False)
+    newprice = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
 
     thumbnail = models.ImageField(upload_to='product/images/', default='static/images/notfound.png')
     images = models.ManyToManyField('ProductImage', related_name='products', blank=True)
+
+    def clean(self):
+        super().clean()
+        if self.sale and self.newprice >= self.price:
+            raise ValidationError("New price must be less than the original price if the product is on sale.")
 
 class ProductImage(models.Model):
     image = models.ImageField(upload_to='product/images/',default='static/images/notfound.png')
