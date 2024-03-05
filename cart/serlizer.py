65a53557a69a03cfcbe7784dcf0a7d89292853d4
“aya <ayahegab2k@gmail.com>
2024-03-05 06:13:13 +0200

    cart app

--- cart/serlizer.py
+++ cart/serlizer.py
@@ -24,15 +24,3 @@ class CartSerlizer(serializers.ModelSerializer):
         # ** means 3aml el validate data k dict
         return Cart.objects.create(**validated_data)
     
-
-    # def update(self, instance, validated_data):
-    #     instance.name = validated_data.get('name')
-    #     instance.image= validated_data.get('image')
-    #     instance.description = validated_data.get('description')
-    #     instance.price = validated_data.get('price')
-    #     instance.add_date = validated_data.get('add_date')
-    #     instance.update_date= validated_data.get('update_date')
-    #     instance.brand = validated_data.get('brand')
-    #     instance.stock= validated_data.get('stock')
-    #     instance.save()
-    #     return instance    
