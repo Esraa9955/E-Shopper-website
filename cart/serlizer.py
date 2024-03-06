5fb62b44eacc06fd007b0962f7c842df76ac6471
“aya <ayahegab2k@gmail.com>
2024-03-06 22:06:15 +0200

    cart app

--- cart/serlizer.py
+++ cart/serlizer.py
@@ -4,6 +4,9 @@ from cart.models import *
 
 class CartSerlizer(serializers.ModelSerializer):
     item_image = serializers.ImageField(source='item.image', read_only=True)
+    item_name = serializers.CharField(source='item.name', read_only=True)
+    item_price = serializers.DecimalField(source='item.price', read_only=True, max_digits=10, decimal_places=2)
+    subtotal_price = serializers.SerializerMethodField()
     def validate(self, data):
         if Cart.objects.filter(user=data['user'], item=data['item']).exists():
             raise serializers.ValidationError({'errmsg':"You have already added this item to your cart."})
@@ -14,6 +17,8 @@ class CartSerlizer(serializers.ModelSerializer):
                 if data['quantity'] > data['item'].stock:
                     raise serializers.ValidationError({'errmsg':"more than stock"})
         return data
+    def get_subtotal_price(self, obj):
+        return obj.quantity * obj.item.price
     class Meta:
         model = Cart
         fields = '__all__'
