2a09dc57b87fafa856f70787c6ef9489653182b4
Esraa Abd El_Maksoud <abdelmaksoudesraa99@gmail.com>
2024-03-07 17:48:43 +0200

    some changesin category

--- category/serializers.py
+++ category/serializers.py
@@ -8,7 +8,12 @@ class SubCategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class CategorySerializer(serializers.ModelSerializer):
-  children = SubCategorySerializer(many=True, read_only=True)
+
   class Meta:
       model = Category
       fields = '__all__'
+  subcategories= serializers.SerializerMethodField(method_name="get_subcategories", read_only=True)
+  def get_subcategories(self,obj):
+        subcategory = obj.chiled.all()
+        serializer = SubCategorySerializer(subcategory,many=True)
+        return serializer.data 
