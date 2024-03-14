cbfa82a5f6e39bac44227b3ea67492546e794623
Esraa Abd El_Maksoud <abdelmaksoudesraa99@gmail.com>
2024-03-14 03:34:42 +0200

    back to store images in database because of quality

--- category/views.py
+++ category/views.py
@@ -12,59 +12,6 @@ class CategoryListCreateView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-
-@api_view(['POST'])
-def Createcategory(request):
-
-
-    serializer = CategorySerializer(data=request.data)
-    serializer.is_valid(raise_exception=True)
-    category = serializer.save()
-    
-    
-    if serializer.is_valid():
-    
-    
-        cat = CategorySerializer(category)
-        # if user send new image
-        if 'image' in request.data and request.data['image'] is not None:
-                # if user has already image on drive
-                if serializer.validated_data.get('image') is not None:
-                    delete_photos(f"{category.id}.png", "1dmBbgr6MYcQpYW02stgKl3ntVkSY9T1h")
-                           
-                # upload new image
-                media_folder = os.path.join(os.getcwd(), "media/category/images/")
-                # save new url
-                Url_Image = upload_photo(os.path.join(media_folder, os.path.basename(serializer['image'].value)),f"{category.id}.png", "1dmBbgr6MYcQpYW02stgKl3ntVkSY9T1h")
-                category.imageUrl = Url_Image
-                category.save()
-                
-                # remove image from server
-                if os.path.exists(media_folder):
-                    for file_name in os.listdir(media_folder):
-                        file_path = os.path.join(media_folder, file_name)
-                        try:
-                            if os.path.isfile(file_path):
-                                os.remove(file_path)
-                                print(f"Deleted: {file_path}")
-                            else:
-                                print(f"Skipped: {file_path} (not a file)")
-                        except Exception as e:
-                            print(f"Error deleting {file_path}: {e}")
-                else:
-                    print("Folder does not exist.")
-
-
-        return Response({"product" : cat.data}, status=status.HTTP_201_CREATED)
-    
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-
-
-
-    
-
 class CategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
@@ -76,3 +23,58 @@ class SubCategoryListCreateView(generics.ListCreateAPIView):
 class SubCategoryRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = SubCategory.objects.all()
     serializer_class = SubCategorySerializer
+
+
+# @api_view(['POST'])
+# def Createcategory(request):
+
+
+#     serializer = CategorySerializer(data=request.data)
+#     serializer.is_valid(raise_exception=True)
+#     category = serializer.save()
+    
+    
+#     if serializer.is_valid():
+    
+    
+#         cat = CategorySerializer(category)
+#         # if user send new image
+#         if 'image' in request.data and request.data['image'] is not None:
+#                 # if user has already image on drive
+#                 if serializer.validated_data.get('image') is not None:
+#                     delete_photos(f"{category.id}.png", "1dmBbgr6MYcQpYW02stgKl3ntVkSY9T1h")
+                           
+#                 # upload new image
+#                 media_folder = os.path.join(os.getcwd(), "media/category/images/")
+#                 # save new url
+#                 Url_Image = upload_photo(os.path.join(media_folder, os.path.basename(serializer['image'].value)),f"{category.id}.png", "1dmBbgr6MYcQpYW02stgKl3ntVkSY9T1h")
+#                 category.imageUrl = Url_Image
+#                 category.save()
+                
+#                 # remove image from server
+#                 if os.path.exists(media_folder):
+#                     for file_name in os.listdir(media_folder):
+#                         file_path = os.path.join(media_folder, file_name)
+#                         try:
+#                             if os.path.isfile(file_path):
+#                                 os.remove(file_path)
+#                                 print(f"Deleted: {file_path}")
+#                             else:
+#                                 print(f"Skipped: {file_path} (not a file)")
+#                         except Exception as e:
+#                             print(f"Error deleting {file_path}: {e}")
+#                 else:
+#                     print("Folder does not exist.")
+
+
+#         return Response({"product" : cat.data}, status=status.HTTP_201_CREATED)
+    
+#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+    
+
+
