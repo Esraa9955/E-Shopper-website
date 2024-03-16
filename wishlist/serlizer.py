cb660c0467d09951672103ee4a86f602ac11cbfc
Esraa Abd El_Maksoud <abdelmaksoudesraa99@gmail.com>
2024-03-16 15:41:31 +0200

    handle cart bug

--- wishlist/serlizer.py
+++ wishlist/serlizer.py
@@ -6,7 +6,10 @@ class WishlistSerlizer(serializers.ModelSerializer):
     item_image = serializers.ImageField(source='item.image', read_only=True)
     item_name = serializers.CharField(source='item.name', read_only=True)
     item_price = serializers.DecimalField(source='item.price', read_only=True, max_digits=10, decimal_places=2)
-    
+    new = serializers.BooleanField(source='item.new', read_only=True)
+    sale = serializers.BooleanField(source='item.sale', read_only=True)
+    stock = serializers.IntegerField(source='item.stock', read_only=True)
+
     def validate(self, data):
         if data['user'].usertype != 'customer':
             raise serializers.ValidationError({'errmsg':"user isn't a customer"})
