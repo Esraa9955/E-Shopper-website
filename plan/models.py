f8806bd81cc815dfc080034aea2548a803ff5127
Hanaa Gamal <elshriefhanaa@gmail.com>
2024-03-26 03:50:02 +0200

    solve bug in plan

--- plan/models.py
+++ plan/models.py
@@ -6,6 +6,7 @@ class Plan(models.Model):
     name=models.CharField(max_length=100)
     price=models.DecimalField(max_digits=10, decimal_places=2)
     description=models.TextField(max_length=1000)
+    count=models.IntegerField(default=0)
     def __str__(self):
         return self.name
     
